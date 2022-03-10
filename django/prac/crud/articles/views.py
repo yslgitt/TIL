c7b00d5d132698764dad78be80cb676a6072ba74
yslgitt <ysltub@gmail.com>
2022-03-11 00:29:28 +0900

    board

---
+++ django/prac/crud/articles/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()[::-1]
+    context = {
+        'articles' : articles,
+    }
+
+    return render(request, 'articles/index.html', context)
+
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request,'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+
+    return redirect('articles:detail',article.pk)
+
+def delete(request,pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == "POST":        
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail',article.pk)
+    
