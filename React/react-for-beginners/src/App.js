0d452f5cb80cdd2255a9a36dc67a857ad4fd600b
yslgitt <ysltub@gmail.com>
2022-07-10 22:20:33 +0900

    react_basic_complete

--- React/react-for-beginners/src/App.js
+++ React/react-for-beginners/src/App.js
@@ -38,5 +38,44 @@ function App() {
     </div>
   );
 }
+function Hello() {
+  // function byeFn() {
+  //   console.log("destroyed :)");
+  // }
+  // function hiFn() {
+  //   console.log("created :)");
+  //   return byeFn;
+  // }
 
-export default App;
+  // useEffect(() => {
+  //   console.log('created :)');
+  //   return () => console.log('destroyed :(');
+  // }, [])
+
+  useEffect(() => {
+    console.log('hi :)');
+    return () => console.log('bye :(')
+  }, []);
+
+  // useEffect(function() {
+  //   console.log('hi :)');
+  //   return function(){
+  //     console.log('bye :(')
+  //   }
+  // }, []);
+
+  return <h1>Hello</h1>;
+}
+
+function App2() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev)
+  return (
+    <div>
+      {showing ? <Hello /> : null }
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App2;
--- React/react-for-beginners/src/index.js
+++ React/react-for-beginners/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import App2 from './App';
 // import "./style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <App />
+  <App2 />
 
 );
