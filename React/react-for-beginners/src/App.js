a86feacb5791f87653d036495d61fdba3b361f04
yslgitt <ysltub@gmail.com>
2022-07-10 21:04:48 +0900

    fix

---
+++ React/react-for-beginners/src/App.js
@@ -0,0 +1,24 @@
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  // const iRunOnlyOnce = () => {
+  //   console.log("i run only once.");
+  // };
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
